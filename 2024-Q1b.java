b3ec85be4a0b11e730fd4d8c3a7b857d928eb4bf
Zhang Yicen <eliza.666888@gmail.com>
2025-02-02 11:29:10 +0800

    Update 2024-Q1b.java

--- 2024-Q1b.java
+++ 2024-Q1b.java
@@ -1,1 +1,34 @@
 
+package pkgfinal.pkg24;
+import java.util.Scanner;
+
+public class Q1b {
+    public static void main(String[] args) {
+        
+        int n;
+        double sum=0.0;
+        Scanner k = new Scanner(System.in);
+        n = k.nextInt();
+        for (int i=1; i<=n; i++){
+            sum += 1.0 / i;
+        }
+        System.out.println(sum);
+    }
+}
+
+/*
+public class Q1b {
+    public static void main(String[] args) {
+        
+        int n;
+        double sum=0.0;
+        Scanner k = new Scanner(System.in);
+        n = k.nextInt();
+        for (int i=1; i<=n; i++){
+            sum += 1/i;
+        }
+        System.out.println(sum);
+    }
+}
+
+*/
